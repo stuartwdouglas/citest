ec9d916bde039b209cafe2700b351a5838f5a164
Stuart Douglas <stuart.w.douglas@gmail.com>
2020-06-10 15:45:50 +1000

    tmp

--- index.js
+++ index.js
@@ -92,9 +92,8 @@ const path = require("path");
         ref: github.context.sha
         }
 
-
-
-        const res = await octokit.checks.listSuitesForRef(req);
+        const res = await octokit.checks.listForRef(req);
+            console.log(JSON.stringify(res))
         const jobName = process.env.GITHUB_JOB
 
         const annotation_level = numFailed + numErrored > 0 ?'failure': 'notice';
